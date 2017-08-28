a595a939220e6ce631e6f04f248a0df83b78b597
Jun Wang <junwang09@gmail.com>
2017-08-27 22:15:52 -0400

    Update nn_model_deep.py

--- nn_model_deep.py
+++ nn_model_deep.py
@@ -33,7 +33,7 @@ def initialize_parameters_deep(layer_dims):
 
     for l in range(1, L):
         ### START CODE HERE ### (≈ 2 lines of code)
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*0.01
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
         ### END CODE HERE ###
         
